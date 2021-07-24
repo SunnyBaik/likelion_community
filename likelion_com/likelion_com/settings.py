f900fb5d6578c0a0916d79a42c1224cfe6c3e3dd
SunnyBaik <qortmdfuf007@hotmail.com>
2021-07-24 15:39:27 +0900

    debug false

--- likelion_com/likelion_com/settings.py
+++ likelion_com/likelion_com/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tjm!w0ir3)=cdlpuisz^3adx2895wkt4^qkek%whi3-avpwn08'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
